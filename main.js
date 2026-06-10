6021c649612965ab3a49267051e5d2d0f4ff8e8e
a <106607480+ariamaranai@users.noreply.github.com>
2026-06-10 14:12:35 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -10,7 +10,7 @@
   let isAutoLike;
   chrome.runtime.sendMessage(0, m => isAutoLike = m);
 
-  let _commentBlock = d.createElement("abbr");
+  let _commentBlock = d.createElement("rb");
   _commentBlock.append("", d.createElement("s"), new Image, "", d.createElement("u"));
 
   let commentFragment = new DocumentFragment;
@@ -85,7 +85,7 @@
       }
       let { childElementCount } = commentFragment;
       isNewest
-        ? (newRoot.insertBefore(commentFragment, childElementCount ? null : newRoot.querySelector("abbr")), endCommentId = firstCommentId)
+        ? (newRoot.insertBefore(commentFragment, childElementCount ? null : newRoot.querySelector("rb")), endCommentId = firstCommentId)
         : childElementCount && newRoot.appendChild(commentFragment);
       resolve();
     });
